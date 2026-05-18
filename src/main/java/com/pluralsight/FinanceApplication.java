534d95adbe09d9cb85463949a8a61b29053549cc
Sveloz03 <velozsam@gmail.com>
2026-05-18 11:06:06 -0400

    created class structure

---
+++ src/main/java/com/pluralsight/FinanceApplication.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import finance.BankAccount;
+
+public class FinanceApplication {
+    static void main() {
+
+        BankAccount account1 = new BankAccount(123, "Pam", 12500);
+        IValuable account2 = new BankAccount(456, "Gary", 1500);
+// try to deposit money into both accounts
+        account1.deposit(100);
+//        account2.deposit(100);
+    }
+}
